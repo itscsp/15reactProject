2abc0128546d6d828818ee6aee28632dc96abe11
itscsp <cspoojary7972@gmail.com>
2022-01-19 07:32:47 +0530

    Add new Project

---
+++ src/Tours.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours, removeTour}) => {
+  return(
+    <section>
+      <div className="title">
+        <h2>ours tours</h2>
+        <div className="underline">
+
+        </div>
+        <div>
+          {tours.map((tour) => {
+
+            return <Tour removeTour={removeTour} key={tour.id} {...tour}></Tour>
+
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
